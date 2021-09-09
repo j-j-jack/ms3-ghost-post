46859592815cb64de6f08560a4ec76a17a38c829
unknown <jackosullivan541@gmail.com>
2021-09-09 13:27:45 +0100

    change app route function to open login.html

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
 
 @app.route("/")
-def test():
-    return "Hello World!"
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
