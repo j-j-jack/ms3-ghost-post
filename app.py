d502e84a0a83b8d5d1b07dd61edb0afed698fdea
unknown <jackosullivan541@gmail.com>
2021-09-10 09:31:04 +0100

    add login functionality to login form

--- app.py
+++ app.py
@@ -22,6 +22,29 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/login",  methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if existing_user:
+            # if password matches user's password in database
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                # The following if/else checks if the user has finished completing their profile.
+                # If they have they are directed to the feed page.
+                # Otherwise they are directed to the finish profile page.
+                if existing_user["profile_complete"] == False:
+                    return render_template("finish-profile.html")
+                else:
+                    return render_template("feed.html")
+            else:
+                # login category added to direct message to login form
+                flash("Incorrect Username and/or Password", "login")
+                return redirect(url_for("login"))
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password", "login")
+            return redirect(url_for("login"))
     return render_template("login.html")
 
 
@@ -33,9 +56,11 @@ def register():
             {"username": request.form.get("username").lower()})
 
         if existing_user:
+            # register category added to direct message to login form
             flash("Username already exists", 'register')
             return redirect(url_for("login"))
 
+        # following code checks if password fields in form match
         password_one = request.form.get("password1")
         password_two = request.form.get("password2")
         if password_one != password_two:
@@ -44,6 +69,8 @@ def register():
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password1")),
+            # setting a new profile_complete value to false.
+            # if user logs back in without finishing profile this value will be checked
             "profile_complete": False
         }
         mongo.db.users.insert_one(register)
