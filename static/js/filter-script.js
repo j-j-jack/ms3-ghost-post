5c8c10c64d1338d3af0a517ccc7c2a837e5cce76
unknown <jackosullivan541@gmail.com>
2021-09-13 08:11:59 +0100

    modify checkbox filter to work on page load

--- static/js/filter-script.js
+++ static/js/filter-script.js
@@ -2,6 +2,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let checkBoxes = document.getElementsByClassName('checkboxes');
     let checkBoxFilter = document.getElementsByClassName('checkbox-filter')[0];
     allCheckbox = document.getElementById('allCheckbox')
+    if(allCheckBox.checked == false){
+            checkBoxFilter.style.background = 'rgba(183, 216, 209, 0)';
+            checkBoxFilter.style.width = '0%';
+            checkBoxFilter.style.height = '0%';
+            checkBoxFilter.style.cursor = 'auto';
+        }
+    else{
+            checkBoxFilter.style.background = 'rgba(0, 0, 0, .3)';
+            checkBoxFilter.style.width = '100%';
+            checkBoxFilter.style.height = '100%';
+            checkBoxFilter.style.cursor = 'not-allowed';
+        }
     document.getElementById('allCheckBox').addEventListener('click', function(){
         if(allCheckBox.checked == false){
             checkBoxFilter.style.background = 'rgba(183, 216, 209, 0)';
