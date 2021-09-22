6b4f0087bf89c97aefeff2e878875f276ab4c820
unknown <jackosullivan541@gmail.com>
2021-09-22 15:52:23 +0100

    add script to click submit button with search icon

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -10,6 +10,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
         let logoutButtonSmall = document.getElementById("logout-button-small");
         logoutButtonSmall.click();
     });
+
+    let searchIconOne = document.getElementsByClassName('search-icon')[0];
+    searchIconOne.addEventListener('click', function(){
+      let searchSubmissionOne=document.getElementsByClassName('search-submission')[0];
+      searchSubmissionOne.click();
+    })
+
+    let searchIconTwo = document.getElementsByClassName('search-icon')[1];
+    searchIconTwo.addEventListener('click', function(){
+      let searchSubmissionTwo=document.getElementsByClassName('search-submission')[1];
+      searchSubmissionTwo.click();
+    })
     $('#burger-icon').click(function(){$('#navbar-container-small').toggle('medium')});
     $('#x-icon').click(function(){$('#navbar-container-small').toggle('medium')});
     const mediaQuery = window.matchMedia('(min-width: 992px)')
