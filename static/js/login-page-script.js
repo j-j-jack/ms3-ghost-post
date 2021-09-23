0546316e7cbd3c3548c5960988314adfd7e2d6f2
unknown <jackosullivan541@gmail.com>
2021-09-23 19:01:31 +0100

    change colors and borders on login page

--- static/js/login-page-script.js
+++ static/js/login-page-script.js
@@ -4,6 +4,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
         loginBox.style.display = 'block';
         let registerBox = document.getElementsByClassName('register-box-small')[0];
         registerBox.style.display = 'none';
+        let registerTitleBox = document.getElementsByClassName("register-title-box")[0];
+        registerTitleBox.style.borderBottom="2px outset black";
+        this.style.borderBottom = "none";
     })
 
     document.getElementsByClassName("register-title-box")[0].addEventListener('click', function(){
@@ -11,5 +14,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
         loginBox.style.display = 'none';
         let registerBox = document.getElementsByClassName('register-box-small')[0];
         registerBox.style.display = 'block';
+        let loginTitleBox = document.getElementsByClassName("login-title-box")[0];
+        loginTitleBox.style.borderBottom="2px outset black";
+        this.style.borderBottom = "none";
     })
 });
